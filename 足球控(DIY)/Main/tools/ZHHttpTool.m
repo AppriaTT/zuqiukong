69d066f1ae8db74b23ccf9bef96e4baaad2c96cc
Arron Yang <yangzhwork@126.com>
2016-04-11 13:50:55 +0800

    修改了网络请求封装

--- 足球控(DIY)/Main/tools/ZHHttpTool.m
+++ 足球控(DIY)/Main/tools/ZHHttpTool.m
@@ -7,46 +7,50 @@
 //
 
 #import "ZHHttpTool.h"
+static AFHTTPSessionManager *_shareManager = nil;
 
 @implementation ZHHttpTool
-+(void)Get:(NSString *)urlString parameters:(NSDictionary *)params acceptableContentTypes:(NSString *)types success:(void(^)(id responseObject))success failure:(void(^)(NSError * error))failure
+
+//懒加载管理者
++(AFHTTPSessionManager *)shareMangager
 {
-//    AFHTTPRequestOperationManager *mgr = [AFHTTPRequestOperationManager manager];
-//    mgr.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:types, nil];
-//    [mgr GET:urlString parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
-//        
-//        success (responseObject);
-//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-//        failure (error);
-//        
-//    }];
-    
-    AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
-    session.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:types, nil];
-    [session GET:urlString parameters:params success:^(NSURLSessionDataTask *task, id responseObject) {
-        success(responseObject);
-    } failure:^(NSURLSessionDataTask *task, NSError *error) {
-        failure(error);
-    }];
+    if (!_shareManager) {
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            
+            _shareManager = [AFHTTPSessionManager manager];
+            // 设置网络超时的时间，10秒。
+            _shareManager.requestSerializer.timeoutInterval = 10;
+            // 设置接收的格式
+            _shareManager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", @"text/html", @"text/xml", @"text/plain", nil];
+        });
+    }
+    return _shareManager;
 }
 
-+(void)Post:(NSString *)urlString parameters:(NSDictionary *)params acceptableContentTypes:(NSString *)types success:(void(^)(id responseObject))success failure:(void(^)(NSError * error))failure
++(void)Get:(NSString *)urlString parameters:(NSDictionary *)params acceptableContentTypes:(NSString *)types success:(void(^)(id responseObject))success failure:(void(^)(NSError * error))failure
 {
-//    AFHTTPRequestOperationManager *mgr = [AFHTTPRequestOperationManager manager];
-//    mgr.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:types, nil];
-//    [mgr POST:urlString parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
-//        success (responseObject);
-//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-//        failure (error);
-//    }];
-    AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
-    session.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:types, nil];
+    AFHTTPSessionManager *session = [self shareMangager];
     
-    [session POST:urlString parameters:params constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+    [session GET:urlString parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        success(responseObject);
         
-    } success:^(NSURLSessionDataTask *task, id responseObject) {
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        failure(error);
+    }];
+    
+}
+
++(void)Post:(NSString *)urlString parameters:(NSDictionary *)params acceptableContentTypes:(NSString *)types success:(void(^)(id responseObject))success failure:(void(^)(NSError * error))failure{
+    
+    AFHTTPSessionManager *session = [self shareMangager];
+    
+    [session POST:urlString parameters:params progress:^(NSProgress * _Nonnull uploadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         success (responseObject);
-    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
         failure (error);
     }];
 }
